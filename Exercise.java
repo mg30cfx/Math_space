dd52070b8bc3e24f19a0cf3bacf88d7b55e34d8f
Andrea Dolzan <mg30cfx@users.noreply.github.com>
2017-07-13 16:38:50 +0200

    Create Exercise.java
    Some sample exercises in Java language

---
+++ Exercise.java
@@ -0,0 +1,13 @@
+import java.lang.Math;
+
+public class Exercise {
+    public static void main(String[] args) {
+	double value1 = 155.55;
+	double value2 = -24.06;
+
+        System.out.println("The ceiling of " + value1 + " is " +
+            Math.ceil(value1));
+        System.out.println("The ceiling of " + value2 + " is " +
+            Math.ceil(value2));
+    }
+}
